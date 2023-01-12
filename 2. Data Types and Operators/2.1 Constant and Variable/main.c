ae3f0a81d22c96902306dcfe72f4c494e1cb1be7
Ruixiang Wang <81059106+ruixiang-wang@users.noreply.github.com>
2023-01-12 16:16:40 +0800

    Add files via upload

---
+++ 2. Data Types and Operators/2.1 Constant and Variable/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define PI 3
+int main() {
+	int a = 3;
+	a = 5;
+	// PI = 10;
+	printf("%d\n", PI);
+}
---
+++ 2. Data Types and Operators/2.3 Decimal conversion/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	/*int i = 123;*/
+	int i = 0x7b;
+	printf("%d\n", i);
+}
